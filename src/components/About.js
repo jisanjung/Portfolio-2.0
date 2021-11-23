091ec0ad80d9ccf8fcc9ec09de69b00dc8c3a7dc
Jisan Jung <jjjung13@gmail.com>
2021-11-23 17:46:28 -0500

    install and implement react slick carousel

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import Title from './Title'
+import React from 'react';
+import Title from './Title';
+import Slider from "react-slick";
 
 const About = () => {
+
+    const settings = {
+        autoplay: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        autoplaySpeed: 2000
+    };
+
     return (
         <section>
             <Title text="About"/>
-            <ul className="text-xl">
+            <ul className="text-2xl">
                 <li>Make it work.</li>
                 <li>Make it right.</li>
                 <li>Make it fast.</li>
@@ -14,6 +28,12 @@ const About = () => {
             The ability to adapt to different environments is a key skill, especially in software development. Because of this, I have grown a passion for learning up-to-date
             technologies and developing my skills which has opened up many opportunities. Here are some companies and startups I've worked for:
             </p>
+            <Slider {...settings}>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+            </Slider>
         </section>
     )
 }
