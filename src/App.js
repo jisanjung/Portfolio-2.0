98618a4c4a96b7615e2adf878ca1a3e77a7ef136
Jisan Jung <jjjung13@gmail.com>
2021-11-22 19:07:53 -0500

    dotted background

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 
 function App() {
+
+  const dottedBackground = {
+    backgroundImage: "radial-gradient(lightgray 1px, transparent 0)",
+    backgroundSize: "7px 7px"
+  }
+
   return (
-    <main className="container mx-auto px-4 text-gray-800 bg-gray-100">
-      <Header/>
-      <Home/>
+    <main className="text-gray-800 bg-gray-100">
+      <div className="container mx-auto px-4" style={dottedBackground}>
+        <Header/>
+        <Home/>
+      </div>
     </main>
   );
 }
