82d383429ccaf94f7d86c8d3496acaa49adb889c
Jisan Jung <jjjung13@gmail.com>
2021-11-24 12:43:19 -0500

    change font

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,10 +8,10 @@ const Projects = () => {
             <Title text="Projects"/>
             <div className="flex mt-8">
                 <VscRecord className="text-sm mr-4 mt-1"/>
-                <div className="flex w-full justify-between">
-                    <h4 className="w-1/5">Filter:</h4>
-                    <div className="w-1/2">
-                        <select name="project" className="w-full">
+                <div className="flex w-full justify-between border-t border-solid border-gray-300">
+                    <h4 className="w-1/5 pl-2">Filter:</h4>
+                    <div className="w-1/2 border-l border-r border-solid border-gray-300">
+                        <select name="project" className="w-full bg-transparent font-bold">
                             <option>All</option>
                         </select>
                     </div>
