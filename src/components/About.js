23ce847de74374de205e79ce315d9c5a2f6d44be
Jisan Jung <jjjung13@gmail.com>
2021-11-24 01:00:59 -0500

    aesthetic icon

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ import paypal from "../images/paypal.png";
 import islandnetworks from "../images/islandnetworks.png";
 import iconplc from "../images/iconplc.png";
 import equisearch from "../images/equisearch.png";
+import { VscRecord } from "react-icons/vsc";
 
 const About = () => {
 
@@ -23,11 +24,14 @@ const About = () => {
     return (
         <section className="py-8">
             <Title text="About"/>
-            <ul className="text-2xl mt-8">
-                <li>Make it work.</li>
-                <li>Make it right.</li>
-                <li>Make it fast.</li>
-            </ul>
+            <div className="flex mt-8">
+                <VscRecord className="text-sm mr-4 mt-1"/>
+                <ul className="text-2xl">
+                    <li>Make it work.</li>
+                    <li>Make it right.</li>
+                    <li>Make it fast.</li>
+                </ul>
+            </div>
             <p className="mt-8">
             The ability to adapt to different environments is a key skill, especially in software development. Because of this, I have grown a passion for learning up-to-date
             technologies and developing my skills which has opened up many opportunities. Here are some companies and startups I've worked for:
