b08a39e210aa8e9b85cbac19bccef537b0d77f4a
Jisan Jung <jjjung13@gmail.com>
2021-11-25 20:22:13 -0500

    fixing some spacing issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import { BsChevronCompactDown, BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Home = () => {
     return (
-        <section className="container mx-auto py-16 h-screen flex justify-center items-center">
+        <section className="container mx-auto h-auto md:h-screen py-28 flex justify-center items-center">
             <div className="flex flex-col items-center">
                 <div className="bg-blue-100 rounded-full w-28 flex justify-center items-center">
                     <img src={memoji} alt="Memoji" className="relative top-1"/>
@@ -14,7 +14,7 @@ const Home = () => {
                     <li><a href="/"><BsLinkedin className="text-gray-800 text-2xl mr-1"/></a></li>
                     <li><a href="/"><BsGithub className="text-gray-800 text-2xl ml-2"/></a></li>
                 </ul>
-                <h3 className="text-2xl text-center py-6">Jisan Jung</h3>
+                <h3 className="text-2xl text-center py-4">Jisan Jung</h3>
                 <h1 className="text-6xl text-center">Software Engineer</h1>
                 <p className="py-8 px-8 text-center">What I love doing most is solving business problems by implementing simple, friendly user interfaces.</p>
                 <Button text="See my work"/>
