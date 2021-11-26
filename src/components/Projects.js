a8e8775ce4f69613039fc6ee589f36ce1e6d1e71
Jisan Jung <jjjung13@gmail.com>
2021-11-25 22:46:07 -0500

    filter projects based on select options

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './Title';
 import { VscRecord } from "react-icons/vsc";
 
 const Projects = () => {
 
-    const projectList = [
+    const [type, setType] = useState("All");
+ 
+    let projectList = [
         {
             id: 1,
-            title: "Pull Request App"
+            title: "Pull Request App",
+            stack: ["ReactJS", "GraphQL", "TailwindCSS", "Github API"],
+            type: "Dashboard"
         },
         {
             id: 2,
-            title: "Glitter and Polish"
+            title: "Glitter and Polish",
+            stack: ["HTML", "CSS", "JavaScript"],
+            type: "Client Site"
         },
         {
             id: 3,
-            title: "Joo's Burgers"
+            title: "Joo's Burgers",
+            stack: ["HTML", "CSS", "JavaScript", "SASS"],
+            type: "Client Site"
         },
         {
             id: 4,
-            title: "COVID-19 Tracker"
+            title: "COVID-19 Tracker",
+            stack: ["ReactJS", "ChartJS", "SASS"],
+            type: "Dashboard"
         },
         {
             id: 5,
-            title: "EquiSearch"
+            title: "EquiSearch",
+            stack: ["ReactJS", "SASS"],
+            type: "Landing Page"
         }
     ];
+
+    projectList = projectList.filter(item => {
+        if (type === "All" || type === "") return projectList
+        return item.type === type;
+    });
 
     return (
         <section className="py-12">
@@ -35,13 +52,14 @@ const Projects = () => {
                 <div className="flex w-full justify-between border-t border-solid border-gray-300">
                     <h4 className="w-1/5 pl-2">Filter:</h4>
                     <div className="w-1/2 border-l border-r border-solid border-gray-300">
-                        <select name="project" className="w-full bg-transparent font-bold">
+                        <select name="project" className="w-full bg-transparent font-bold" onChange={e => setType(e.target.value)}>
                             <option>All</option>
                             <option>Dashboard</option>
+                            <option>Client Site</option>
                             <option>Landing Page</option>
                         </select>
                     </div>
-                    <span className="w-1/4">4 items</span>
+                    <span className="w-1/4">{projectList.length} items</span>
                 </div>
             </div>
             <ul className="mt-8">
