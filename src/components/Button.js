31c3bdb4b46a0507b65f40a68360670f742e4766
jisanjung <jjjung13@gmail.com>
2021-11-29 22:45:17 -0500

    changed button to accept children

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = (props) => {
     return (
         <button className="text-white bg-gray-800 py-2 px-8 rounded-full">
-            {props.text}
+            {props.children}
         </button>
     )
 }
