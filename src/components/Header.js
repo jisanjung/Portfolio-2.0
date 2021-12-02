cee97232dccf745eb33ee49d4f30e8d6612182b7
jisanjung <jjjung13@gmail.com>
2021-12-01 20:38:26 -0500

    resize logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <section className="bg-gray-100 absolute left-0 right-0">
             <div className="flex justify-between">
-                <a href="/" className="text-2xl h-12 px-6 bg-green-600 text-white tracking-wider flex items-center"><img src={logo_bold} alt="Logo" className="w-12"/></a>
+                <a href="/" className="text-2xl h-12 px-6 bg-green-600 text-white tracking-wider flex items-center"><img src={logo_bold} alt="Logo" className="w-10"/></a>
                 <ul className="flex items-center py-4">
                     <li className="pl-4"><a href="/">About</a></li>
                     <li className="pl-4"><a href="/">Projects</a></li>
