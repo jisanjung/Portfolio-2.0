ca03f11c188a4f160eee32f77ae11da125f40950
jisanjung <jjjung13@gmail.com>
2021-12-01 21:59:03 -0500

    added content inside footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import logo_black from "../images/logo_black.png";
 
 const Footer = () => {
     return (
-        <section>
-            
+        <section className="py-12">
+            <div className="flex justify-center">
+                <img src={logo_black} alt="Logo" className="w-12"/>
+            </div>
+            <h3 className="text-center mt-2">&copy; {new Date().getFullYear()} Jisan Jung</h3>
         </section>
     )
 }
