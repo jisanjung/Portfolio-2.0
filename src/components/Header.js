e5a9820ab3ed46450dd098f33853bdd0b3be87a3
jisanjung <jjjung13@gmail.com>
2021-12-01 20:37:04 -0500

    replace text with logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { MdSubdirectoryArrowLeft } from "react-icons/md";
+import logo_bold from "../images/logo_bold.png";
 
 const Header = () => {
     return (
         <section className="bg-gray-100 absolute left-0 right-0">
             <div className="flex justify-between">
-                <a href="/" className="text-2xl h-12 px-6 bg-green-600 text-white tracking-wider flex items-center"><span>JJ</span></a>
+                <a href="/" className="text-2xl h-12 px-6 bg-green-600 text-white tracking-wider flex items-center"><img src={logo_bold} alt="Logo" className="w-12"/></a>
                 <ul className="flex items-center py-4">
                     <li className="pl-4"><a href="/">About</a></li>
                     <li className="pl-4"><a href="/">Projects</a></li>
