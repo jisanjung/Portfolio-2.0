745c30eaf2110fd7fa9b3245667f60107bcb599d
Jisan Jung <jjjung13@gmail.com>
2021-12-07 23:17:44 -0500

    adjust footer in bigger sizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <section className="py-12">
-            <h1 className="logo text-center text-2xl">jjung</h1>
+        <section className="py-12 lg:pb-16">
+            <h1 className="logo text-center text-2xl xl:text-4xl xl:mb-4">jjung</h1>
             <h3 className="text-center mt-2">&copy; {new Date().getFullYear()} Jisan Jung</h3>
         </section>
     )
