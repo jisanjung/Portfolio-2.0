584418ef0d0a12404e0a44ad986e8f3f95fd3c17
Jisan Jung <jjjung13@gmail.com>
2021-12-07 22:10:11 -0500

    make title bigger at xl size

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import { MdSubdirectoryArrowLeft } from "react-icons/md";
 
 const Title = (props) => {
     return (
-        <h1 className="text-6xl md:text-8xl flex items-center">
+        <h1 className="text-6xl md:text-8xl xl:text-9xl flex items-center">
             <span>{props.text}</span>
             <MdSubdirectoryArrowLeft className="transform -rotate-90 text-xl md:text-2xl relative top-3 left-2"/>
         </h1>
