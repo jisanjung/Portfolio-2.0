57b4f2caa93b64cfb9dd38e147c5a13ed59ffdf2
Jisan Jung <jjjung13@gmail.com>
2021-12-07 22:18:49 -0500

    text and padding changes in about xl width

--- src/components/About.js
+++ src/components/About.js
@@ -25,20 +25,20 @@ const About = () => {
         <section className="pt-12 pb-28 md:pb-36 lg:pb-48">
             <Title text="About"/>
             <div className="flex flex-col md:flex-row">
-                <div className="flex mt-8 md:mt-12 w-auto md:w-2/5">
+                <div className="flex mt-8 md:mt-12 xl:mt-14 w-auto md:w-2/5">
                     <VscRecord className="text-sm md:text-base mr-4 mt-1"/>
-                    <ul className="text-2xl md:text-3xl">
+                    <ul className="text-2xl md:text-3xl xl:text-4xl">
                         <li>Make it work.</li>
                         <li>Make it right.</li>
                         <li>Make it fast.</li>
                     </ul>
                 </div>
                 <div className="w-auto md:w-3/5">
-                    <p className="mt-8 md:mt-12 md:text-lg">
+                    <p className="mt-8 md:mt-12 xl:mt-14 md:text-lg xl:text-xl">
                     The ability to adapt to different environments is a key skill, especially in software development. Because of this, I have grown a passion for learning up-to-date
                     technologies and developing my skills which has opened up many opportunities. Here are some companies and startups I've worked for:
                     </p>
-                    <div className="mt-8">
+                    <div className="mt-8 xl:mt-12">
                         <Slider {...settings}>
                             <img src={paypal} alt="PayPal logo" className="px-3"/>
                             <img src={islandnetworks} alt="Island Networks logo" className="px-3"/>
