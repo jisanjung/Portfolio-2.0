bda9486f5d21e7fe223f1f8469304e579bf3b58b
Jisan Jung <jjjung13@gmail.com>
2021-12-07 19:03:02 -0500

    layout change in contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,12 +12,12 @@ const Contact = () => {
                 <VscRecord className="text-sm mr-4 mt-1"/>
                 <h3 className="text-2xl">Let's talk.</h3>
             </div>
-            <div className="flex flex-col mt-12">
-                <div>
+            <div className="flex flex-col md:flex-row mt-12">
+                <div className="w-full md:w-1/2">
                     <h4 className="flex items-center text-gray-500"><GrMailOption className="mr-3 text-lg"/>jisan.jung@temple.edu</h4>
                     <h4 className="flex items-center mt-2 text-gray-500"><GrLocationPin className="mr-3 text-lg"/>Philadelphia, PA</h4>
                 </div>
-                <form className="mt-12">
+                <form className="mt-12 md:mt-auto w-full md:w-1/2">
                     <div className="mb-8">
                         <label htmlFor="name" className="block text-sm font-bold">Name</label>
                         <input type="text" name="name" className="bg-transparent border-b border-gray-400 w-full py-2" placeholder="Full name"/>
