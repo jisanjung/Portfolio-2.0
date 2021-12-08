65a9a64d6bdbe1d79c39bf900c4213903c155b49
Jisan Jung <jjjung13@gmail.com>
2021-12-07 22:25:00 -0500

    change arrow size

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = (props) => {
     return (
         <h1 className="text-6xl md:text-8xl xl:text-9xl flex items-center">
             <span>{props.text}</span>
-            <MdSubdirectoryArrowLeft className="transform -rotate-90 text-xl md:text-2xl relative top-3 left-2"/>
+            <MdSubdirectoryArrowLeft className="transform -rotate-90 text-xl md:text-2xl xl:text-3xl relative top-3 left-2 xl:top-4 xl:left-3"/>
         </h1>
     )
 }
